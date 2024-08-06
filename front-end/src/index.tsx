a71bde9de18d3a12cf09f0872f7b94a627491808
Tobias Agyasta <agyasta1808@gmail.com>
2024-08-06 19:51:10 +0700

    Feat : Added accordion

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -5,7 +5,7 @@ import "./styles/index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import SignIn from "./pages/SignIn";
-
+import { AccordionDemo } from "./pages/Accordion";
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
@@ -15,6 +15,7 @@ root.render(
 			<Routes>
 				<Route path="/" element={<App />} />
 				<Route path="/signin" element={<SignIn />} />
+				<Route path="/accordion" element={<AccordionDemo />} />
 			</Routes>
 		</Router>
 	</React.StrictMode>
