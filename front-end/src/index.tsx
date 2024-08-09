bc38fdb182d1956504c13213d193a7159d370a13
Tobias Agyasta <agyasta1808@gmail.com>
2024-08-09 19:48:47 +0700

    Feat : Connected current pages

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -11,21 +11,17 @@ import AgricultureEcommerce from "./pages/mainPage";
 import SignUp from "./pages/SignUp";
 import CompanyProfile from "./pages/CompanyProfile";
 
-
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-
 	<React.StrictMode>
 		<Router>
 			<Routes>
-				<Route path="/" element={<App />} />
+				<Route path="/" element={<AgricultureEcommerce />} />
 				<Route path="/signin" element={<SignIn />} />
-        <Route path="/mainpage" element={<AgricultureEcommerce />} />
 				<Route path="/signup" element={<SignUp />} />
 				<Route path="/companyprofile" element={<CompanyProfile />} />
-
 			</Routes>
 		</Router>
 	</React.StrictMode>
