d22dbd3a5b5c505ea77ae1301d9fbebb5de337f3
Dimas <adhi.pratama20@gmail.com>
2024-08-24 11:18:26 +0700

    responsive main page(header,footer)

--- front-end/src/components/HarvestHub/Footer.tsx
+++ front-end/src/components/HarvestHub/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
   return (
     <footer className="bg-white p-8 shadow mt-8">
       <div className="container mx-auto flex justify-between items-start">
-        <div>
+        <div className="mb-6 md:mb-0">
           <h3 className="text-lg font-bold mb-2">Follow Us</h3>
           <div className="space-x-4">
             <a href="#" className="text-gray-700">
@@ -25,9 +25,9 @@ const Footer = () => {
             </a>
           </div>
         </div>
-        <div>
+        <div className="mb-6 md:mb-0">
           <h3 className="text-lg font-bold mb-2">HarvestHub</h3>
-          <ul className="text-gray-700">
+          <ul className="text-gray-700 space-y-2">
             <li>
               <a href="#">Blog</a>
             </li>
@@ -42,9 +42,9 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div>
+        <div className="mb-6 md:mb-0">
           <h3 className="text-lg font-bold mb-2">Jual</h3>
-          <ul className="text-gray-700">
+          <ul className="text-gray-700 space-y-2">
             <li>
               <a href="#">Pusat Edukasi Seller</a>
             </li>
@@ -53,9 +53,9 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div>
+        <div className="mb-6 md:mb-0">
           <h3 className="text-lg font-bold mb-2">Bantuan dan Panduan</h3>
-          <ul className="text-gray-700">
+          <ul className="text-gray-700 space-y-2">
             <li>
               <a href="#">HarvestHub Care</a>
             </li>
--- front-end/src/components/HarvestHub/Header.tsx
+++ front-end/src/components/HarvestHub/Header.tsx
@@ -8,116 +8,111 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import ShoppingCartIcon from "../cards/ShoppingCartIcon";
 
 const Header = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+  const navigate = useNavigate();
 
-	const [cartOpen, setCartOpen] = useState(false);
-	const [user, setUser] = useState<User | null>(null);
-	const navigate = useNavigate();
+  const toggleCart = () => {
+    setCartOpen(!cartOpen);
+  };
 
-	const toggleCart = () => {
-		setCartOpen(!cartOpen);
-	};
+  useEffect(() => {
+    const fetchUser = async () => {
+      const currentUser = await fetchCurrentUser();
+      setUser(currentUser);
+    };
+    fetchUser();
+  }, []);
 
-	useEffect(() => {
-		const fetchUser = async () => {
-			const currentUser = await fetchCurrentUser();
-			setUser(currentUser);
-		};
-		fetchUser();
-	}, []);
+  return (
+    <nav className="bg-white shadow p-2 flex flex-col md:flex-row justify-between items-center sticky top-0 z-50">
+      <Link to="/">
+        <div className="text-xl font-semibold">
+          <img
+            src="../images/logo.png"
+            alt="HarvestHub Logo"
+            className="h-8 w-auto"
+          />
+        </div>
+      </Link>
 
-	return (
-		<nav className='bg-white shadow p-4 flex justify-between items-center sticky top-0 z-50'>
-			<Link to='/'>
-				<div className='text-2xl font-bold'>
-					<img
-						src='../images/logo.png'
-						alt='HarvestHub Logo'
-						className='h-10 w-auto'
-					/>
-				</div>
-			</Link>
+      <div className="relative w-[250px] sm:w-[300px] md:w-[400px]">
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full border border-gray-300 px-3 py-1 pl-8 rounded-full text-sm focus:outline-none"
+        />
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <FontAwesomeIcon icon={faSearch} className="text-gray-500 text-sm" />
+        </div>
+      </div>
 
-			<div className='flex items-center space-x-4'>
-				<div className='relative w-[400px]'>
-					<input
-						type='text'
-						placeholder='Search'
-						className='border border-gray-400 px-4 py-2 pl-10 rounded-full focus:outline-none'
-					/>
-					<div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
-						<FontAwesomeIcon icon={faSearch} className='text-gray-500' />
-					</div>
-				</div>
-			</div>
+      <div className="flex space-x-2">
+        <Link to="/companyprofile">
+          <button className="text-gray-700 border border-gray-300 rounded px-3 py-1 text-sm hover:text-blue-500 hover:shadow-md transition-shadow duration-300">
+            Tentang HarvestHub
+          </button>
+        </Link>
+      </div>
 
-			<div className='space-x-4'>
-				<Link to='/companyprofile'>
-					<button className='text-gray-700 border border-gray-300 rounded px-4 py-2 hover:text-blue-500 hover:shadow-md transition-shadow duration-300'>
-						Tentang HarvestHub
-					</button>
-				</Link>
-			</div>
-
-			{user ? (
-				<>
-					<div className='flex flex-row items-center justify-center gap-4'>
-						<h1>Welcome {user.name}!</h1>
-						<Avatar>
-							<AvatarImage
-								src={`${apiBaseUrl}/uploads/${user?.profile_picture_url}`}
-							/>
-							<AvatarFallback>{user.name[0]}</AvatarFallback>
-						</Avatar>
-
-						{/* Conditional rendering based on user role */}
-						{user.role === "seller" ? (
-							<Link to='/sellerpage'>
-								<button className='bg-green-500 text-white px-4 py-2 rounded hover:shadow-md transition-shadow duration-300'>
-									Seller Page
-								</button>
-							</Link>
-						) : (
-							<>
-								<ShoppingCartIcon isOpen={cartOpen} toggleCart={toggleCart} />
-								<Link to='/buyerpage'>
-									<button className='bg-blue-500 text-white px-4 py-2 rounded hover:shadow-md transition-shadow duration-300'>
-										Buyer Page
-									</button>
-								</Link>
-							</>
-						)}
-
-						<button
-							onClick={() => {
-								logout();
-								navigate("/");
-								setUser(null);
-							}}
-							className='bg-gray-400 text-white px-4 py-2 rounded hover:shadow-md transition-shadow duration-300'
-						>
-							Logout
-						</button>
-					</div>
-				</>
-			) : (
-				<>
-					<div className='space-x-4'>
-						<Link to='/signin'>
-							<button className='bg-gray-200 px-4 py-2 rounded hover:shadow-md transition-shadow duration-300'>
-								Masuk
-							</button>
-						</Link>
-						<Link to='/signup'>
-							<button className='bg-black text-white px-4 py-2 rounded hover:shadow-md transition-shadow duration-300'>
-								Daftar
-							</button>
-						</Link>
-					</div>
-				</>
-			)}
-		</nav>
-	);
-
+      {user ? (
+        <>
+          <div className="flex items-center space-x-2">
+            {" "}
+            <span className="text-sm">Welcome {user.name}!</span>
+            <Avatar>
+              <AvatarImage
+                src={`${apiBaseUrl}/uploads/${user?.profile_picture_url}`}
+                className="h-6 w-6"
+              />
+              <AvatarFallback>{user.name[0]}</AvatarFallback>
+            </Avatar>
+            {user.role === "seller" ? (
+              <Link to="/sellerpage">
+                <button className="bg-green-500 text-white px-3 py-1 rounded text-sm hover:shadow-md transition-shadow duration-300">
+                  Seller Page
+                </button>
+              </Link>
+            ) : (
+              <>
+                <ShoppingCartIcon isOpen={cartOpen} toggleCart={toggleCart} />
+                <Link to="/buyerpage">
+                  <button className="bg-blue-500 text-white px-3 py-1 rounded text-xs md:text-sm hover:shadow-md transition-shadow duration-300">
+                    Buyer Page
+                  </button>
+                </Link>
+              </>
+            )}
+            <button
+              onClick={() => {
+                logout();
+                navigate("/");
+                setUser(null);
+              }}
+              className="bg-gray-400 text-white px-3 py-1 rounded text-sm hover:shadow-md transition-shadow duration-300"
+            >
+              Logout
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex items-center space-x-2">
+            <Link to="/signin">
+              <button className="bg-gray-200 px-3 py-1 rounded text-sm hover:shadow-md transition-shadow duration-300">
+                Masuk
+              </button>
+            </Link>
+            <Link to="/signup">
+              <button className="bg-black text-white px-3 py-1 rounded text-sm hover:shadow-md transition-shadow duration-300">
+                Daftar
+              </button>
+            </Link>
+          </div>
+        </>
+      )}
+    </nav>
+  );
 };
 
 export default Header;
